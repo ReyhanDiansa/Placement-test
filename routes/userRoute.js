42438def1969481fd7fa92624939db9124f5135a
ReyhanDiansa <reyhandiansa@gmail.com>
2023-10-27 16:47:45 +0700

    update user route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,14 +5,12 @@ const app = express();
 app.use(express.json());
 
 const userController = require("../controllers/userController");
-const auth = require(`../auth/auth`);
 require("dotenv/config");
 
 
 app.post("/login", userController.Login);
 app.post(
   "/register",
-  auth.authVerify,
   userController.Register
 );
 
